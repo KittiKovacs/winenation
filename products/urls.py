610bafe21c2d5e46a8bf9426d01ca6d21234e888
Kitti Kovacs <bambula86@gmail.com>
2021-02-15 20:10:28 +0000

    Added event and subscription templates and views

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    path('<product_id>', views.product_details, name='product_details'),
+    path('', views.all_events, name='events'),
+    path('<event_id>', views.event_details, name='event_details'),
+    path('', views.all_subscriptions, name='subscriptions'),
+    path('<subscription_id>', views.subscription_details,
+         name='subsciption_details'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Event, Subscription
 
 # Create your views here.
@@ -14,3 +14,60 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_details.html', context)
+
+
+def all_events(request):
+    """ A view to show all events, including sorting and search queries """
+
+    events = Event.objects.all()
+
+    context = {
+        'events': events,
+    }
+
+    return render(request, 'events/events.html', context)
+
+
+def event_details(request, event_id):
+
+    event = get_object_or_404(Event, pk=event_id)
+
+    context = {
+        'event': event,
+    }
+
+    return render(request, 'events/event_details.html', context)
+
+
+def all_subscriptions(request):
+    """ A view to show all events, including sorting and search queries """
+
+    subscriptions = Subscription.objects.all()
+
+    context = {
+        'subscriptions': subscriptions,
+    }
+
+    return render(request, 'products/subscriptions.html', context)
+
+
+def subscription_details(request, subscription_id):
+
+    subscription = get_object_or_404(Subscription, pk=subscription_id)
+
+    context = {
+        'subscription': subscription,
+    }
+
+    return render(request, 'subscriptions/subscription_details.html', context)
