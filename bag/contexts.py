ba7d5ef6406955c3f4bd4fca4b186102e18a7666
Kitti Kovacs <bambula86@gmail.com>
2021-02-23 21:04:35 +0000

    Updated bag template and context

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Wine, Subscription, Event
 
 
 def bag_contents(request):
@@ -7,6 +9,23 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        wine = get_object_or_404(Wine, pk=item_id)
+        subscription = get_object_or_404(Subscription, pk=item_id)
+        event = get_object_or_404(Event, pk=item_id)
+        total += (quantity * wine.price)+(
+                  quantity*event.price)+(
+                  quantity*subscription.price)
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'wine': wine,
+            'subscription': subscription,
+            'event': event,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- bag/views.py
+++ bag/views.py
@@ -7,17 +7,17 @@ def view_bag(request):
     return render(request, 'bag/bag.html')
 
 
-def add_to_bag(request, wine_id):
+def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-    if wine_id in list(bag.keys()):
-        bag[wine_id] += quantity
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
     else:
-        bag[wine_id] = quantity
+        bag[item_id] = quantity
 
     request.session['bag'] = bag
     print(request.session['bag'])
