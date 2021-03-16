3e2a61a5c5121fcefb49bf41e29249c00158ecb9
Kitti Kovacs <bambula86@gmail.com>
2021-03-16 20:43:53 +0000

    Complete checkout form and add country dropdown

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -93,12 +93,11 @@ class StripeWH_Handler:
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for quantity in item_data:
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
                                 quantity=quantity,
-                                product_size=size,
                             )
                             order_line_item.save()
             except Exception as e:
@@ -108,13 +107,12 @@ class StripeWH_Handler:
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            content=f'Webhook received: {event["type"]} | \
+                SUCCESS: Created order in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
-        """
-        Handle the payment_intent.payment_failed webhook from Stripe
-        """
+
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
