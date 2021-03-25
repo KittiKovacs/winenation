101785a154d2382fdc3650e9d3e11c37caaa2140
Kitti Kovacs <bambula86@gmail.com>
2021-03-25 20:27:54 +0000

    Add more tests to products

--- bag/tests_views.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-from django.urls import reverse, resolve
-from bag.views import (view_bag, add_to_bag, adjust_bag, remove_from_bag)
-
-
--- products/models.py
+++ products/models.py
@@ -54,9 +54,9 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True,
                                  blank=True, on_delete=models.SET_NULL)
     winery = models.CharField(max_length=254, null=True,
-                                 blank=True)
+                              blank=True)
     country = models.CharField(max_length=254, null=True,
-                                 blank=True)
+                               blank=True)
     variety = models.ForeignKey('Variety', null=True, blank=True,
                                 on_delete=models.SET_NULL, max_length=254)
     vintage = models.IntegerField(null=True, blank=True)
